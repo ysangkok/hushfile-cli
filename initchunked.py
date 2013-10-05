7a3a614f6461e7a8236daea4fcecceb4384bd2d6
Janus <ysangkok@gmail.com>
2013-10-05 16:26:57 +0200

    write metadata too, strip newlines

--- initchunked.py
+++ initchunked.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
-import requests, sys
+import requests, sys, json
 from config import endpoint
 
-if len(sys.argv) < 2:
-    print("Usage: " + sys.argv[0] + " <number of chunks>", file=sys.stderr)
+if len(sys.argv) < 3:
+    print("Usage: " + sys.argv[0] + " <number of chunks> <encrypted metadata>", file=sys.stderr)
     sys.exit(1)
+
+metadata = sys.stdin.buffer.read().decode("utf-8")
 
 print(
     requests.post(
         "{}/upload".format(endpoint), 
-        data={'metadata': 'metadata','deletepassword': 'deletepassword', "numberofchunks": sys.argv[1]}, 
+        data={'metadata': sys.argv[2], 'deletepassword': json.loads(metadata)["deletepassword"], "numberofchunks": sys.argv[1]}, 
     ).text
 )
--- moren.py
+++ moren.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-import sys, subprocess, os, json, shlex, time
+import sys, subprocess, os, json, shlex, time, tempfile
 from math import ceil
 from traceback import print_exc
+from subprocess import PIPE
+import base64
 
 if len(sys.argv) < 3:
     print("Usage: " + sys.argv[0] + " <file> <passphrase>", file=sys.stderr)
@@ -19,22 +21,26 @@ if numberchunks > 30:
     print("File too large or chunks too small. Number chunks: " + str(numberchunks), file=sys.stderr)
     sys.exit(1)
 
-chunkpipes = ["chunk{}".format(str(i).zfill(len(str(numberchunks-1)))) for i in range(numberchunks)]
+chunkpipes = [os.path.join(tempfile.gettempdir(), "chunk{}".format(str(i).zfill(len(str(numberchunks-1))))) for i in range(numberchunks)]
 
 for i in chunkpipes:
     os.mkfifo(i)
 
 try:
-    c = subprocess.check_output(["./initchunked.py", str(numberchunks)])
-    fileid = json.loads(c.decode("utf-8"))["fileid"]
+    metadata = json.dumps({"filename": os.path.basename(infilepath), "mimetype": "", "filesize": "-1", "deletepassword": "deletepassword"}).encode("utf-8")
+    with subprocess.Popen(["openssl", "enc", "-aes-256-cbc", "-pass", "pass:" + passphrase, "-e"], stdin=PIPE, stdout=PIPE) as p2:
+        encmetadata = base64.b64encode(p2.communicate(metadata)[0])
+    with subprocess.Popen(["./initchunked.py", str(numberchunks), encmetadata.strip()], stdin=PIPE, stdout=PIPE) as p:
+        c = p.communicate(metadata)[0]
+        fileid = json.loads(c.decode("utf-8"))["fileid"]
     
     cmd = ["./multi_progress.py"] + [json.dumps(["./addchunk.py", chunksize, fileid, x[0], x[1]]) for x in enumerate(chunkpipes)]
     #print(" ".join(shlex.quote(x) for x in cmd), file=sys.stderr)
     #input("continue?")
-    p = subprocess.Popen(" ".join(shlex.quote(x) for x in cmd) + "&", shell=True, stderr=subprocess.PIPE)
+    p = subprocess.Popen(" ".join(shlex.quote(x) for x in cmd) + "&", shell=True, stderr=PIPE)
     time.sleep(1)
 
-    subprocess.check_call("cat " + shlex.quote(infilepath) + " | openssl enc -aes-256-cbc -pass pass:" + shlex.quote(passphrase) + " -e -base64 | ./distribute_over_pipes.py " + str(chunksize) + " " + " ".join([shlex.quote(i) for i in chunkpipes]) + "", shell=True)
+    subprocess.check_call("cat " + shlex.quote(infilepath) + " | openssl enc -aes-256-cbc -pass pass:" + shlex.quote(passphrase) + " -e -base64 | tr -d '\n' | ./distribute_over_pipes.py " + str(chunksize) + " " + " ".join([shlex.quote(i) for i in chunkpipes]) + "", shell=True)
 
     p.wait(timeout=5)
     stderr = p.stderr.read().decode("utf-8")
